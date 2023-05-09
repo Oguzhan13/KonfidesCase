84dbd9e1590b391012b708da804f5e8672ad494b
Oğuzhan <oguzhanbayram13@gmail.com>
2023-05-09 15:32:38 +0300

    View Model class library kullanılmadığından silindi

--- KonfidesCase.ViewModel/Admin/NewCategoryVM.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KonfidesCase.ViewModel.Admin
-{
-    public class NewCategoryVM
-    {
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- KonfidesCase.ViewModel/CategoriesVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KonfidesCase.ViewModel
-{
-    public class CategoriesVM
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
