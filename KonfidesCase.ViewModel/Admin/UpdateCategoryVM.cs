3c34a2e9e51f3609c2c162a6e4024d4db2357443
Oguzhan13 <oguzhanbayram13@gmail.com>
2023-05-09 15:23:51 +0300

    View model kütüpanesi silindi

--- KonfidesCase.ViewModel/Admin/UpdateCategoryVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KonfidesCase.ViewModel.Admin
-{
-    public class UpdateCategoryVM
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
